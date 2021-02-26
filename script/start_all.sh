a81cfa7990ce453467556614d57b9ea5995349c8
Trần Tuấn Cảnh <39213510+connortran216@users.noreply.github.com>
2021-02-26 16:03:25 +0700

    Update start_all.sh

--- script/start_all.sh
+++ script/start_all.sh
@@ -4,7 +4,7 @@ docker-compose \
     -f docker-compose.stream.yml \
     -f docker-compose.database.yml \
     -f docker-compose.admin.yml up -d \
-    --scale detector=3 \
-    --scale embedder=3 \
-    --scale register-response-handler=2 \
-    --scale verifier-response-handler=2 \
+#     --scale detector=3 \
+#     --scale embedder=3 \
+#     --scale register-response-handler=2 \
+#     --scale verifier-response-handler=2 \
