e52ef361646291054d596d5bb49163bdbd896302
Trần Tuấn Cảnh <39213510+connortran216@users.noreply.github.com>
2021-02-26 16:23:35 +0700

    Update build.sh

--- script/build.sh
+++ script/build.sh
@@ -4,6 +4,6 @@ if [ -z $(docker network ls --filter name=^${DOCKER_INTERNAL_NETWORK}$ --format=
      docker network create ${DOCKER_INTERNAL_NETWORK} ;
 fi
 
-bash script/stop_all.sh
+# bash script/stop_all.sh
 
 bash script/start_all.sh
